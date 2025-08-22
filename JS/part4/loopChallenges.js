f569d7162a3d48b14b45cb5b1a95be84ca20a718
qwerty-arun <arunkr.1804@gmail.com>
2025-08-22 12:56:40 +0530

    arrays and while loops

---
+++ JS/part4/loopChallenges.js
@@ -0,0 +1,23 @@
+/*
+1. Write a 'while' loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable name 'sum'.
+*/
+
+let sum = 0;
+let i = 1;
+while (i <= 5) {
+ sum += i;
+ i++;
+}
+// console.log(sum);
+
+/*
+2. Write a 'while' loop that counts down from 5 to 1 and stores the numbers in an array named 'countdown'
+*/
+
+let j = 5;
+let countdown = []
+while(j>=1){
+    countdown.push(j);
+    j--;
+}
+console.log(countdown);

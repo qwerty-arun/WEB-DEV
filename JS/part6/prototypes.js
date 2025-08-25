869cb00972128adf0497ed4a9dee824b0a74eaa6
qwerty-arun <arunkr.1804@gmail.com>
2025-08-26 01:23:45 +0530

    prototypes, oops, functional constructors etc. comparing oops and prototype based programming

---
+++ JS/part6/prototypes.js
@@ -0,0 +1,26 @@
+let computer = { cpu: 12 };
+let lenovo = {
+  screen: "HD",
+  __proto__: computer,
+};
+let hw = {};
+
+// console.log(`lenovo `, lenovo.__proto__);
+
+let genericCar = { tyres: 4 };
+
+let tesla = {
+  driver: "AI",
+};
+
+Object.setPrototypeOf(tesla, genericCar);
+console.log(`tesla `, tesla);
+console.log(`genericCar`, genericCar);
+console.log(`tesla tyres`, tesla.tyres);
+console.log(`tesla `, Object.getPrototypeOf(tesla));
+
+//these are good checks
+console.log(`tesla hasOwnProperty driver?`, tesla.hasOwnProperty("driver"));
+console.log(`tesla hasOwnProperty tyres?`, tesla.hasOwnProperty("tyres"));
+console.log(`tesla hasOwn driver?`, Object.hasOwn(tesla, "driver"));
+console.log(`tesla hasOwn tyres?`, Object.hasOwn(tesla, "tyres"));

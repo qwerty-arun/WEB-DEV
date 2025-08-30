96526692c62e5c99d4f1db9f2f65ccdfc5b86e00
qwerty-arun <arunkr.1804@gmail.com>
2025-08-31 00:01:03 +0530

    closures, promises, protypal inheritance, this and context binding

---
+++ JS/part11_adv/this_binding_context.js
@@ -0,0 +1,17 @@
+const person = {
+  name: "hitesh",
+  greet() {
+    console.log(`Hi, I am ${this.name}`);
+  },
+};
+
+person.greet();
+
+const greetFn = person.greet;
+greetFn();
+
+// const boundGreet = person.greet.bind({ name: "John" });
+const boundGreet = person.greet.bind({name: `${person.name}`});
+boundGreet();
+
+// bind, call and apply

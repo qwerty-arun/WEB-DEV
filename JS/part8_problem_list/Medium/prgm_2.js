e6adb0dad8560a668fc00ef7754ba556a143f5c5
qwerty-arun <arunkr.1804@gmail.com>
2025-09-06 13:39:46 +0530

    small changes in fibonacci program

--- JS/part8_problem_list/Medium/prgm_2.js
+++ JS/part8_problem_list/Medium/prgm_2.js
@@ -2,8 +2,11 @@
 
 // O(2^n)
 function fibonacci(n) {
-  if (n <= 1) {
-    return n; // Base cases: F(0) = 0, F(1) = 1
+  if (n == 1) {
+    return 0; // Base cases: F(0) = 0, F(1) = 1
+  }
+  if (n == 2) {
+    return 1;
   }
   return fibonacci(n - 1) + fibonacci(n - 2); // Recursive case
 }
@@ -11,13 +14,12 @@ function fibonacci(n) {
 // Example usage:
 console.log(fibonacci(6)); // Output: 8
 
-
 // Optimized approach using memoization, O(n)
 function fibonacci1(n, memo = {}) {
   if (n in memo) return memo[n]; // Return cached result
   if (n <= 1) return n; // Base cases
 
-  memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo); // Store result
+  memo[n] = fibonacci1(n - 1, memo) + fibonacci1(n - 2, memo); // Store result
   return memo[n];
 }
 
